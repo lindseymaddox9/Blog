37ece5e6824bc98116daccd5563c6140f3f5c417
lindseymaddox9 <153481027+lindseymaddox9@users.noreply.github.com>
2024-07-03 18:02:46 -0500

    began navigation bar and added toggle to front page

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Challenge 4 Blog
 Blog
 Description-Challenge 4 Blog
-Technologies used-JavaScript ES 13 ,CSS 3, and HTML 5
+Technologies used-JavaScript ES 13 ,CSS 3, tailwind, and HTML 5
 Usage: learning how to showcase skills
-Credits: Collaboration with Jose Lopez, MD, W3 schools
+Credits: MD, W3 schools, Chat GTP, 
 Liscense-MIT in repo
--- assets/js/form.js
+++ assets/js/form.js
@@ -62,4 +62,21 @@ blogForm.addEventListener('submit', function(event) {
     }
 });
 
+const themeToggleBtn = document.querySelector("#darkmode");
+
+themeToggleBtn.addEventListener("click", () => {
+  const mode = localStorage.getItem("mode") || "dark";
+  
+  let newMode;
+
+  if (mode === "dark") {
+    newMode = "light";
+    document.body.classList = 'light'
+  } else {
+    newMode = "dark";
+    document.body.classList = 'dark'
+  }
+
+  localStorage.setItem("mode", newMode);
+});
 
